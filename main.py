59cfb947ea22ee3eac0fcec946710a6131433481
danielgois82 <76977240+danielgois82@users.noreply.github.com>
2025-03-28 15:59:57 -0300

    Aula 2 de ETL, slide 5 DS

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
-import requests
 import pandas as pd
+from src.extractTransform import requestAPIBCB
+from src.load import salvarCSV
 
-def requestAPIBCB(data):
-    '''
-    Requisicao da API do BCB sobre dados publicos sobre meios de pagamentos \n
-    Parametro: data - string, format AAAAT, ex: 20191
-    '''
-    url = f"https://olinda.bcb.gov.br/olinda/servico/MPV_DadosAbertos/versao/v1/odata/MeiosdePagamentosTrimestralDA(trimestre=@trimestre)?@trimestre='{data}'&$format=json"
-    req = requests.get(url)
-    # print(req.status_code)
-    dados = req.json()
-    df = pd.json_normalize(dados['value'])
-    return print(df)
+dados_bcb = requestAPIBCB('20191')
 
-requestAPIBCB('20241')
+salvarCSV(dados_bcb, 'ETL_BCB/src/datasets/meiosPagamentosTri.csv', ';', '.')
