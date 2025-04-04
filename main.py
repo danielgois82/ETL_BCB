4929a57e5b96366de5f7837568186b01dfaa8a06
danielgois82 <76977240+danielgois82@users.noreply.github.com>
2025-04-04 15:57:41 -0300

    Alterações da aula de Data Science do dia 04/04/2025

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import pandas as pd
 from src.extractTransform import requestAPIBCB
-from src.load import salvarCSV
+from src.load import salvarCSV, salvarSQLite, salvarMySQL
 
-dados_bcb = requestAPIBCB('20191')
+dados_bcb = requestAPIBCB("20191")
 
-salvarCSV(dados_bcb, 'src/datasets/meiosPagamentosTri.csv', ';', '.')
+# salvarCSV(dados_bcb, 'src/datasets/meiosPagamentosTri.csv', ';', '.')
+
+# salvarSQLite(dados_bcb, 'src/datasets/etlbcb.db', 'meios_pagamentos_tri')
+
+salvarMySQL(dados_bcb, "root", "root", "localhost", "etlbcb", "meios_pagamento_tri")
--- src/extractTransform.py
+++ src/extractTransform.py
@@ -1,16 +1,17 @@
 import requests
 import pandas as pd
 
+
 def requestAPIBCB(data: str) -> pd.DataFrame:
-    '''
+    """
     Requisicao da API do BCB sobre dados publicos referente a meios de pagamentos \n
     Parametro: Data - string, formato AAAAT, ex: 20191 \n
     Saida: Um Pandas DataFrame
-    '''
+    """
     url = f"https://olinda.bcb.gov.br/olinda/servico/MPV_DadosAbertos/versao/v1/odata/MeiosdePagamentosTrimestralDA(trimestre=@trimestre)?@trimestre='{data}'&$format=json"
     req = requests.get(url)
-    print('Status code:', req.status_code)
+    print("Status code:", req.status_code)
     dados = req.json()
-    df = pd.json_normalize(dados['value'])
-    df['datatrimestre'] = pd.to_datetime(df['datatrimestre'])
+    df = pd.json_normalize(dados["value"])
+    df["datatrimestre"] = pd.to_datetime(df["datatrimestre"])
     return df
